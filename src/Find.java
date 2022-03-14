b2755f313437f8dc0ca816969662b06985627f59
JamesDavies-exe <daviesjamesemlyn@gmail.com>
2022-03-14 16:54:26 +0100

    Initial commit

---
+++ src/Find.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+
+public class Find {
+    private String text;
+    public Find(String text) {
+        this.text = text;
+    }
+    public boolean match(String pattern) {
+        return false;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
